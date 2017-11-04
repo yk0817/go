7ca802985e023a902464b53df35a6104bbe0eee1
ken_yamamoto <ken.yamamoto@coincheck.com>
2017-11-04 22:42:39 +0900

    途中

---
+++ day01/variable.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 変数定義、代入
+	var n1 int = 100
+	n2 := 100
+	var (
+		n3 = 100
+		n4 = 100
+	)
+
+	fmt.Println(n1)
+	fmt.Println(n2)
+	fmt.Println(n3)
+	fmt.Println(n4)
+}
