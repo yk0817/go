144a09908a404ca0a88940c659a55279af1d30e1
ken_yamamoto <ken.yamamoto@coincheck.com>
2017-11-04 17:11:11 +0900

    frist commit

---
+++ day01/func_definition.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var n int = 100
+
+func main() {
+	fmt.Println(add(2, 1))
+	fmt.Println(swap(2, 1))
+	fmt.Println(n)
+}
+
+func add(x, y int) int {
+	return x + y
+}
+
+func swap(x, y int) (int, int) {
+	return y, x
+}
