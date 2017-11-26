548fc81ee7dbb1170ae7c28c281f25a57247143d
ken_yamamoto <ken.yamamoto@coincheck.com>
2017-11-26 13:55:16 +0900

    勉強 memo: blog用に後でスクリプトを自分で書いてみる。

---
+++ study_grammer/methods_and_interface/indirection.go
@@ -0,0 +1,21 @@
+package main
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func ScaleFunc(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	v := Vertex{3, 4}
+	v.Scale(2)
+	ScaleFunc(&v, 10)
+}
