a03f213c3c8ab559f70686809d0b19faeb48e844
山本憲 <ken.yamamoto@coincheck.com>
2017-12-13 23:03:40 +0900

    Go query first

--- goquery/scraping_go.go
+++ goquery/scraping_go.go
@@ -1,23 +1,20 @@
 package main
 
-// 
-// 写経
-// https://qiita.com/yosuke_furukawa/items/5fd41f5bcf53d0a69ca6#goquery%E3%82%92%E4%BD%BF%E3%81%86
-
 import (
-  "fmt"
-  "github.com/PuerkitoBio/goquery"
+	"fmt"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-func GetPage(url string)  {
-  doc, _ := goquery.NewDocument(url)
-  doc.Find("a").Each(func(_ int, s *goquery.Selection) {
-    url, _ := s.Attr("href")
-    fmt.Println(url)
-  })
+func GetPage(url string) {
+	doc, _ := goquery.NewDocument(url)
+	doc.Find("a").Each(func(_ int, s *goquery.Selection) {
+		url, _ := s.Attr("href")
+		fmt.Println(url)
+	})
 }
 
-func main()  {
-  url := "http://qiita.com/advent-calendar/2013/"
-  GetPage(url)
+func main() {
+	url := "http://qiita.com/advent-calendar/2013/"
+	GetPage(url)
 }
